bc5d4f923ef640008582d7b425cffc674b64f4bf
hakam <arrafhakam@gmail.com>
2015-12-01 17:10:28 +0700

    scnd commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=json',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
---
+++ models/Employee.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "employee".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $bod
+ * @property string $note
+ *
+ * @property Phone[] $phones
+ */
+class Employee extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'employee';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['bod'], 'safe'],
+            [['name', 'note'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'bod' => 'Bod',
+            'note' => 'Note',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPhones()
+    {
+        return $this->hasMany(Phone::className(), ['employee_id' => 'id']);
+    }
+}
